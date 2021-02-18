f1cb5c95537c211a4eadf1343accff07da1ff82b
MrSlavik <76463749+MrSlavik0@users.noreply.github.com>
2021-02-18 17:40:38 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+class connect {
+    constructor (key) {
+            try {
+            this.api = async function (method, params = {}) {
+                params.token = key;
+                let p = [];
+                Object.keys(params).map(x => {
+                    p.push(`${x}=${params[x]}`);
+                });
+                p = p.join('&');
+                let result = fetch.default("http://api.vasyabot.xyz/" + method + "/?" + p).then(e => e.text());
+                return result;
+            }
+        } catch (err) {
+            return {
+                error: {
+                    text: "METHOD NOT FOUND"
+                }
+            }
+        }
+    }
+}
+
+module.exports = connect;
